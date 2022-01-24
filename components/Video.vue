8422cc7cedcc7710579a0541606ef9713399c14e
Teri Eyenike <teyenike1@gmail.com>
2022-01-24 13:19:22 +0100

    Add: module nuxt/cloudinary installed

---
+++ components/Video.vue
@@ -0,0 +1,12 @@
+<template>
+  <p class="text-slate-800 text-5xl">teri eyenike</p>
+</template>
+
+<script>
+export default {
+  name: "VideoPlayer",
+};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Video />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
